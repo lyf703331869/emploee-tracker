111b3dd19ec3e2e3feee2fb0f8333f9d72714ac2
lyf703331869 <yliu52@nyit.edu>
2022-06-15 17:04:36 -0400

    Change the seeds file, show him/herself if he/she is a leader

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,12 +18,12 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("John", "Doe", 1 , NULL),
+    ("John", "Doe", 1 , 1),
     ("Mike", "Chan", 2 , 1),
-    ("Ashley", "Rodriguez", 3 , NULL),
+    ("Ashley", "Rodriguez", 3 , 3),
     ("Kevin", "Tupik", 4 , 3),
-    ("Kunal", "Singh", 5 , NULL),
+    ("Kunal", "Singh", 5 , 5),
     ("Malia", "Brown", 6 , 5),
-    ("Sarah", "Lourd", 7 , NULL),
+    ("Sarah", "Lourd", 7 , 7),
     ("Tom", "Allen", 8 , 7);
 
